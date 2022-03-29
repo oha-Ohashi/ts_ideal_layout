ffaf28e0e4b61a1e32529ab6d7b923d5257ac384
Ohashi_uhbie <tmatomatox1@gmail.com>
2022-03-29 19:38:15 +0900

    daikaikaku

--- src/search.ts
+++ src/search.ts
@@ -29,7 +29,20 @@ class Random {
 }
 const random = new Random(1);
 
+/*
+export function searchAllInOne(): void{
+	let nk: io.NaiKeeper = new io.NaiKeeper("./src/config.txt")
 
+	for(let i = 0; i < 10; i++){
+		const indv: string = search(nk)
+		const keys_to_show = nk.mizuni_modosu(indv.split(""))
+		console.log(keys_to_show)
+		console.log(ev.keys3x10(keys_to_show))
+		console.log(ev.evaluate(keys_to_show).cost_sum)
+	}
+}
+*/
+/*
 export function search(nk: io.NaiKeeper): string{
 	const n_roop: number= 2000
 	let abc: string[] = nk.nai
@@ -51,6 +64,42 @@ export function search(nk: io.NaiKeeper): string{
 	}
 	return abc.join("")
 }
+*/
+export class Search{
+	nk: io.NaiKeeper
+	text: string
+	result: string
+	constructor(config: string, text: string){
+		this.nk = new io.NaiKeeper(config)
+		this.text = text
+		this.result = this.nk.mizuni_modosu(this.nk.nai)
+		
+	}
+	doit(): string{
+		const n_roop: number = 2000
+		let abc: string[] = this.nk.nai
+		let e_max: number = ev.evaluate(
+			this.nk.mizuni_modosu(abc), this.text
+		).cost_sum
+		for(let i = 0; i < n_roop; i++){
+			const new_abc: string[] = swap(abc)
+			const new_e: number = ev.evaluate(
+				this.nk.mizuni_modosu(new_abc), this.text
+			).cost_sum
+			if(new_e < e_max){
+				abc = new_abc
+				e_max = new_e
+				this.result = this.nk.mizuni_modosu(abc)
+			}
+		}
+		return abc.join("")
+	}
+	show(): void{
+		console.log(this.result)
+		console.log(ev.keys3x10(this.result))
+		console.log(ev.evaluate(this.result, this.text).cost_sum)
+	}
+}
 
 function swap(arg: string[]){
 	let arr:string[] = arg.map(x => x)
@@ -61,12 +110,18 @@ function swap(arg: string[]){
 	arr[j] = tmp
 	return arr
 }
-function getRandomInt(max: number) {
-	return Math.floor(Math.random() * max);
-}
+
 
 if(require.main === module) {
-	let nk: io.NaiKeeper = new io.NaiKeeper("./src/config.txt")
+	const s = new Search(
+		"xxxx__xxxxxxKxxxxxxxxHJL__xxxx",
+		"it is a fine dayIt was on a dreary night of November that I beheld the accomplishment of my toils. With an anxiety that almost amounted to agony, I collected the instruments of life around me, that I might infuse a spark of being into the lifeless thing that lay at my feet. It was already one in the morning; the rain pattered dismally against the panes, and my candle was nearly burnt out, when, by the glimmer of the half-extinguished light, I saw the dull yellow eye of the creature open; it breathed hard, and a convulsive motion agitated its limbs.How can I describe my emotions at this catastrophe, or how delineate the wretch whom with such infinite pains and care I had endeavoured to form? His limbs were in proportion, and I had selected his features as beautiful. Beautiful! Great God! His yellow skin scarcely covered the work of muscles and arteries beneath; his hair was of a lustrous black, and flowing; his teeth of a pearly whiteness; but these luxuriances only formed a more horrid contrast with hkonaida roan roan kun ni a xtu ta toki 、 maruzen no mise de 一 niti ni mannenhitu ga nani honi u reru daro u to tazu ne tara 、 roan kun ha oo i toki ha hyakumoto kurai de ru sou da to kota e ta 。 otto sore de ha 一 hon no mannenhitu ga dono kurai naga ku tuka eru daro u to ki i tara 、 konaida yokohama no mono de 、 pen ha mada ka nari da ga 、 ziku zi kuga he xtu ta kara ziku take dake kae kae te kure kure to i xtu te mo xtu te ki ta no ga aru ga 、 konohito ha 十三 nen mae ni 一 hon ka xtu ta giri de 、 sono 一 hon wo kyou made ta e zu siyou si te i ta no da to iu kara 、 kore kore ga maa itiban naga i rei rasii to hana si ta 。 si te mi ru to hutuu no baai de ha ikura zankoku ni tuka xtu te mo taitei 六七 nen no hosyou ha tu ke rareru no ga 、 ippan no mannenhitu no unmei rasii 。 一 hon de otto hodo sorehodo naga ku tuka eru mono ga hi ni hyakumoto mo de ru to i e ba mannenhitu wo zyuyou suru hito no hani ha hizyou na ikioi wo omonmi moxtu te hiro gari tutu aru to mi te mo manzara manzara kentoutiga"
+	)
+	s.doit()
+	s.show()
+
+	/*
+	let nk: io.NaiKeeper = new io.NaiKeeper("xxxx__xxxxxxKxxxxxxxxHJL__xxxx")
 
 	for(let i = 0; i < 10; i++){
 		const indv: string = search(nk)
@@ -75,6 +130,7 @@ if(require.main === module) {
 		console.log(ev.keys3x10(keys_to_show))
 		console.log(ev.evaluate(keys_to_show).cost_sum)
 	}
+	*/
 }
 
 
