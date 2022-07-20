0a7dee7113f429d30a85231e9bbc5f2cef092cb6
Ohashi_uhbie <tmatomatox1@gmail.com>
2022-07-20 11:36:32 +0900

    add Tomisuke

--- src/evaluate.ts
+++ src/evaluate.ts
@@ -44,6 +44,8 @@ if(require.main === module) {
 	disp_eval("QWFPBJLUY_ARSTGMNEIOZXCDVKH___", text)
 	console.log("Layout: Eucalyn")
 	disp_eval("QW___MRDYPAOEIUGTKSNZXCVFBHJL_", text)
+	console.log("Layout: Tomisuke")
+	disp_eval("1,.-;LRDYPAOEIUGNTSKXCVWQJHMBZ", text)
 	console.log("Layout: Ohashi rev1")
 	disp_eval("_WBF_MRDYPAOEUIGTKNSZXCV__HJLQ", text)
 	console.log("Layout: Ohashi rev2")
